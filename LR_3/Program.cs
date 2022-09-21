f7e1e4404f0ffab5803b498d40a64dbc2dd59546
FawazKabir <91149679+FawazKabir@users.noreply.github.com>
2022-09-21 15:07:07 +0300

    Add files via upload

---
+++ LR_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LR_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числитель>> ");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите знаменатель>> ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int nod;
+            int a = M;
+            int b = N;
+            while ((b > 0) && (a > 0))
+            {
+                int tmp = a;
+                a = b;
+                b = tmp % b;
+            }
+            nod = a;
+            if (nod > 0)
+            {//сокращение
+                M /= nod;
+                N /= nod;
+            }
+            Console.WriteLine("Дробь сокращена");
+            Console.WriteLine(M);
+            Console.WriteLine("-");
+            Console.WriteLine(N);
+            Console.ReadKey();
+
+        }
+    }
+}
